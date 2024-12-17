b558586be00dc304f3373172d8ab2deb783a609f
kourosh <kourosh23@hotmail.com>
2024-12-17 17:58:04 +0100

    routes

---
+++ src/routes/catalog.ts
@@ -0,0 +1,7 @@
+import { Express } from "express";
+
+export const createCatalogRoutes = (app: Express) => {
+    app.get("/", (req, resp) => {
+        resp.send("Hello, SportsStore Route");
+    })
+}
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import { createServer } from "http";
 import express, { Express } from "express";
 import helmet from "helmet";
 import { getConfig } from "./config";
+import { createRoutes } from "./routes";
 
 const port = getConfig("http:port", 5000);
 
@@ -11,9 +12,7 @@ expressApp.use(helmet());
 expressApp.use(express.json());
 expressApp.use(express.urlencoded({ extended: true }));
 
-expressApp.get("/", (req, resp) => {
-    resp.send("Hello, SportsStore");
-});
+createRoutes(expressApp);
 
 const server = createServer(expressApp);
 server.listen(port, () => console.log(`HTTP Server listening on port ${port}`));

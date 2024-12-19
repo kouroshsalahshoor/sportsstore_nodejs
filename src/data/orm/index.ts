ee04e2c9e8fd16715ddc4e7d51060fee89997039
kourosh <kourosh23@hotmail.com>
2024-12-19 14:02:07 +0100

    ch 16

---
+++ src/data/orm/index.ts
@@ -0,0 +1,8 @@
+import { CatalogRepository } from "../catalog_repository";
+import { BaseRepo } from "./core";
+import { AddQueries } from "./queries";
+import { AddStorage } from "./storage";
+
+const RepoWithQueries = AddQueries(BaseRepo);
+const CompleteRepo = AddStorage(RepoWithQueries);
+export const CatalogRepoImpl = CompleteRepo;

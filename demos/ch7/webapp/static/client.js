ff562225c5e5106a0617b3766be78effd685a381
kourosh <kourosh23@hotmail.com>
2024-12-25 12:01:31 +0100

    ch7

---
+++ demos/ch7/webapp/static/client.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("btn").addEventListener("click", sendReq);
+});
+
+const requestUrl = "/read";
+
+sendReq = async () => {
+    //let payload = [];
+    //for (let i = 0; i < 5; i++) {
+    //    payload.push({ id: i, message: `Payload Message: ${i}\n` });
+    //}
+    const response = await fetch(requestUrl, {
+        method: "POST", body: JSON.stringify(payload),
+        //headers: {
+        //    "Content-Type": "application/json"
+        //}
+    })
+    document.getElementById("msg").textContent = response.statusText;
+    document.getElementById("body").textContent = `Resp: ${await response.text()}`;
+}
